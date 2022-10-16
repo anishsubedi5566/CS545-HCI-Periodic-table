f7237e3b086f10a487a0a2b747c4328d93a3e60e
adityaparab04 <adityaparab04@gmail.com>
2022-10-16 15:51:17 -0400

    libraries and packages installed plus periodic table component created

--- periodic-table-frontend/src/App.js
+++ periodic-table-frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import PeriodicTable from './components/PeriodicTable';
 
 function App() {
   return (
     <div className="App">
       <h1>Periodic Table</h1>
+      <PeriodicTable />
     </div>
   );
 }
---
+++ periodic-table-frontend/src/components/PeriodicTable.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../App.css";
+const PeriodicTable = () => {
+    return(
+        <div>
+            <h2>Using Elements</h2>
+        </div>
+    )
+}
+
+export default PeriodicTable;
