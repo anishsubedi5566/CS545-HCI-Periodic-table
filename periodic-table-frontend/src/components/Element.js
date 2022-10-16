ddd7be8df3c2ba306677d7aa85321ec99ea749aa
adityaparab04 <adityaparab04@gmail.com>
2022-10-16 16:05:44 -0400

    search and element component file created

---
+++ periodic-table-frontend/src/components/Element.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css';
+
+const Element = () => {
+    return(
+        <div>
+            <p>Element</p>
+        </div>
+    )
+}
+
+export default Element;
---
+++ periodic-table-frontend/src/components/SearchElements.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../App.css';
+
+const SearchElements = () => {
+    return(
+        <div>
+            <p>SearchElements</p>
+        </div>
+    )
+}
+
+export default SearchElements;
