7fbd9229bf7c2cefd928012530751cd61e5a0cb8
Rahul Pawar <rahulpawar166@gmail.com>
2022-11-25 22:07:37 -0500

    CSS changes, search component and search element added

--- periodic-table-frontend/src/components/context-api/reducer.js
+++ periodic-table-frontend/src/components/context-api/reducer.js
@@ -1,9 +1,32 @@
+import data from "../../PeriodicTableJSON.json";
 export const initialState = {
-    elementsOpacity: 1
+    elements: data.elements,
+    colorMap: {
+        "noble gas": "#FFBC42",
+        "alkaline earth metal": "#808000",
+        "diatomic nonmetal": "#D81159",
+        "polyatomic nonmetal": "#FF4500",
+        "alkali metal": "#8F2D56",
+        "transition metal": "#191970",
+        "post-transition metal": "#218380",
+        "lanthanide": "#9400D3",
+        "actinide": "#EE82EE",
+        "metalloid": "#73D2DE",
+        "unknown, probably transition metal": "#191970",
+        "unknown, probably post-transition metal": "#218380",
+        "unknown, probably metalloid": "#73D2DE",
+        "unknown, but predicted to be an alkali metal": "#8F2D56",
+        "unknown, predicted to be noble gas": "#FFBC42"
+    },
+    elementsOpacity: 1,
+    periodicSearch: "hidebx",
+    searchList: data.elements,
 }
 
 export const actionTypes = {
-    SET_ELEMENT_OPACITY: 'SET_ELEMENT_OPACITY'
+    SET_ELEMENT_OPACITY: 'SET_ELEMENT_OPACITY',
+    SEARCH_UI_TOGGLE: 'SEARCH_UI_TOGGLE',
+	SEARCH_LIST: 'SEARCH_LIST',
 }
 
 const reducer = (state, action) => {
@@ -12,6 +35,16 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 elementsOpacity: action.elementsOpacity,
+            }
+        case actionTypes.SEARCH_UI_TOGGLE:
+            return {
+                ...state,
+                periodicSearch: action.periodicSearch,
+            }
+        case actionTypes.SEARCH_LIST:
+            return {
+                ...state,
+                searchList: action.searchList,
             }
         default:
             return state
